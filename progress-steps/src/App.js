862fe000d28ff405d0197c045517223d56ff7db7
facuevas <franciscuevas4@gmail.com>
2021-03-20 17:50:32 -0700

    Completed Day 2

--- progress-steps/src/App.js
+++ progress-steps/src/App.js
@@ -1,8 +1,50 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
-  const [index, setIndex] = useState(1);
+  const [current, setCurrent] = useState(1);
+  const [prevBtnState, setPrevBtnState] = useState(true);
+  const [nextBtnState, setNextBtnState] = useState(false);
+
+  const decrementIndex = () => {
+    setCurrent(current - 1);
+  };
+
+  const incrementIndex = () => {
+    setCurrent(current + 1);
+  };
+
+  const updateButtonState = () => {
+    if (current > 1 && current < 4) {
+      setNextBtnState(false);
+      setPrevBtnState(false);
+    } else if (current === 1) {
+      setPrevBtnState(true);
+    } else if (current === 4) {
+      setNextBtnState(true);
+    }
+  };
+
+  const updateActiveCircles = () => {
+    const circles = document.querySelectorAll(".circle");
+
+    circles.forEach((circle, index) => {
+      if (index < current) {
+        circle.classList.add("active");
+      } else {
+        circle.classList.remove("active");
+      }
+    });
+
+    const progressBar = document.getElementById("progress");
+
+    progressBar.style.width = ((current - 1) / (4 - 1)) * 100 + "%";
+  };
+
+  useEffect(() => {
+    updateButtonState();
+    updateActiveCircles();
+  });
 
   return (
     <div className="App">
@@ -14,10 +56,20 @@ const App = () => {
           <div className="circle">3</div>
           <div className="circle">4</div>
         </div>
-        <button className="btn" id="prev" disabled={false}>
+        <button
+          className="btn"
+          id="prev"
+          disabled={prevBtnState}
+          onClick={decrementIndex}
+        >
           Prev
         </button>
-        <button className="btn" id="next">
+        <button
+          className="btn"
+          id="next"
+          disabled={nextBtnState}
+          onClick={incrementIndex}
+        >
           Next
         </button>
       </div>
